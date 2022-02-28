412088d42f25b3673d45ef79b945308b32da05b7
muhammadnurulahsan <mdnurulahsanbdbd@gmail.com>
2022-02-28 15:20:29 +0600

    new code added

--- countries.js
+++ countries.js
@@ -10,13 +10,28 @@ const displayCountries = (countries) => {
   const countriesDiv = document.getElementById("countries");
   countries.forEach((country) => {
     const div = document.createElement("div");
-    div.classList.add('country')
-    const h3 = document.createElement("h3");
-    h3.innerText = country.name;
-    div.appendChild(h3);
-    const p = document.createElement("p");
-    p.innerText = country.capital;
-    div.appendChild(p);
+    div.classList.add("country");
+    div.innerHTML = `
+    <h3>${country.name}</h3>
+    <p>${country.capital}</p>
+    <button onclick="loadCountryByName('${country.name}')">Show Details</button>
+    `;
     countriesDiv.appendChild(div);
   });
+};
+
+const loadCountryByName = (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayCountriesDetails(data[0]));
+};
+const displayCountriesDetails = (country) => {
+  const countryDiv = document.getElementById("country-details");
+  countryDiv.innerHTML = `
+  <h3>${country.name.common}</h3>
+  <p>Population: ${country.population}</p>
+    <img width="200px" src="${country.flags.png}" alt="">
+  
+  `;
 };
---
+++ mealdb.js
@@ -0,0 +1,72 @@
+const searchFood = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  searchField.value = "";
+  if(searchText == "") {
+    alert("Please enter a search term");
+    return;
+  }
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displaySearchResult(data.meals));
+};
+
+const displaySearchResult = (meals) => {
+  const searchResult = document.getElementById("search-result");
+  searchResult.textContent = "";
+  if (meals.length == 0) {
+    alert("No result found");
+    return;
+  }
+  
+  meals.forEach((meal) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+    <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+        <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+        </div>
+    </div>`;
+    searchResult.appendChild(div);
+  });
+};
+
+const loadMealDetail = (mealId) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetails(data.meals[0]));
+};
+
+const displayMealDetails = (meal) => {
+  console.log(meal);
+  const mealDetails = document.getElementById("meal-details");
+  const div = document.createElement("div");
+  div.classList.add("card");
+  div.innerHTML = `
+    <div class="row">
+    <div class="col-md-4">
+        <img src="${
+          meal.strMealThumb
+        }" class="img-fluid rounded-start" alt="..." />
+    </div>
+    <div class="col-md-8">
+        <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 900)}</p>
+        <p class="card-text">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </p>
+        <a target="_blank" href="${
+          meal.strYoutube
+        }" class="btn btn-primary shadow-none">Go somewhere</a>
+        </div>
+    </div>
+    </div>
+    `;
+  mealDetails.appendChild(div);
+};
